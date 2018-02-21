08ba786deed490887c6cac29fd4288bfc0a10bd9
blendedmarket <nate@blendedmarket.com>
2018-02-20 22:12:17 -0700

    First commit

--- src/server.js
+++ src/server.js
@@ -2,16 +2,37 @@
 const path = require('path')
 const express = require('express')
 const layout = require('express-layout')
-
+const bodyParser = require('body-parser')
+const validator = require('express-validator')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+const flash = require('express-flash')
+const helmet = require('helmet')
+const csrf = require('csurf')
 const routes = require('./routes')
 const app = express()
+
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 const middlewares = [
   layout(),
-  express.static(path.join(__dirname, 'public'))
+  helmet(),
+  express.static(path.join(__dirname, 'public')),
+  bodyParser.urlencoded(),
+  validator(),
+  cookieParser(),
+  session({
+    secret: 'super-secret-key',
+    key: 'super-secret-cookie',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 60000 }
+  }),
+  flash(),
+  csrf({ cookie: true })
+
 ]
 app.use(middlewares)
 
