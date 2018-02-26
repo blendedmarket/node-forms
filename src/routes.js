af557824f5fbf8e16c8682d8edb7ed9c2f359af2
blendedmarket <nate@blendedmarket.com>
2018-02-25 20:41:50 -0700

    created forms 1 and 2

--- src/routes.js
+++ src/routes.js
@@ -87,4 +87,102 @@ router.post('/contact', [
     res.redirect('/thanks')
   })
 
+
+      router.get('/form1', (req, res) => {
+        sessionID = uuid();
+        // visitor.pageview("/contact", "http://e-sql.com/contact", "Contact Form").send();
+        res.render('form1', {
+          data: {},
+          errors: {},
+          csrfToken: req.csrfToken(),
+          uuid: req.uuid, //n8 uuid added
+          formIDCustom: '100',
+          sessionID: sessionID
+        })
+      })
+
+      router.post('/form1', [
+        check('name')
+          .isLength({ min: 1 })
+          .withMessage('Please type something in the message box...')
+          .trim(),
+        check('email')
+          .isEmail()
+          .withMessage('Are you sure that is an email...?')
+          .trim()
+          .normalizeEmail()
+        ], (req, res) => {
+          const errors = validationResult(req)
+          if(errors.isEmpty()){
+            // console.log("Errors is empty : ", errors.isEmpty())
+          }
+          if (!errors.isEmpty()) {
+            visitor.event("Event Category", "validationFailEvent").send()
+            return res.render('form1', {
+              data: req.body,
+              errors: errors.mapped(),
+              csrfToken: req.csrfToken(),
+              uuid: req.uuid, //n8 uuid added
+              formIDCustom: '100',
+              sessionID: sessionID
+            })
+          }
+
+          const data = matchedData(req)
+          console.log('Cleaned up data for form 1: ', data)
+          // console.log('req: ', req)
+
+          req.flash('success', 'Submit is many successes from form 1! :)')
+          res.redirect('/thanks')
+        })
+
+
+              router.get('/form2', (req, res) => {
+                sessionID = uuid();
+                // visitor.pageview("/contact", "http://e-sql.com/contact", "Contact Form").send();
+                res.render('form2', {
+                  data: {},
+                  errors: {},
+                  csrfToken: req.csrfToken(),
+                  uuid: req.uuid, //n8 uuid added
+                  formIDCustom: '200',
+                  sessionID: sessionID
+                })
+              })
+
+              router.post('/form2', [
+                check('name')
+                  .isLength({ min: 1 })
+                  .withMessage('Please type something in the message box...')
+                  .trim(),
+                check('email')
+                  .isEmail()
+                  .withMessage('Are you sure that is an email...?')
+                  .trim()
+                  .normalizeEmail()
+                ], (req, res) => {
+                  const errors = validationResult(req)
+                  if(errors.isEmpty()){
+                    // console.log("Errors is empty : ", errors.isEmpty())
+                  }
+                  if (!errors.isEmpty()) {
+                    visitor.event("Event Category", "validationFailEvent").send()
+                    return res.render('form2', {
+                      data: req.body,
+                      errors: errors.mapped(),
+                      csrfToken: req.csrfToken(),
+                      uuid: req.uuid, //n8 uuid added
+                      formIDCustom: '200',
+                      sessionID: sessionID
+                    })
+                  }
+
+                  const data = matchedData(req)
+                  console.log('Cleaned up data form 2: ', data)
+                  // console.log('req: ', req)
+
+                  req.flash('success', 'Submit is many successes with form 2! :)')
+                  res.redirect('/thanks')
+                })
+
 module.exports = router
